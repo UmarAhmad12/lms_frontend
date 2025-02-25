70fc5a6383f1cd857d585dcdec86833f3e8e570b
UmarAhmad12 <umara6274@gmail.com>
2025-02-25 20:33:29 +0530

    Fixing error134

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -5,6 +5,6 @@ const BASE_URL = "https://lms-backend-sage-ten.vercel.app/api/v1";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = false;
+axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
